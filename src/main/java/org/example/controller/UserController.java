7ffffc5ee38a6fdd067b807244b364cce0f7099e
AuroraMitl <68555321+AuroraMitl@users.noreply.github.com>
2022-04-06 00:05:03 +0400

    Initial commit

---
+++ src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,42 @@
+package org.example.controller;
+
+
+import org.example.model.User;
+import org.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String findAll(Model model){
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "user-list";
+    }
+
+    @GetMapping("/users-create")
+    public String createUserForm(User user){
+        return "user-create";
+    }
+
+    @PostMapping
+    public String createUser(User user){
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+}
